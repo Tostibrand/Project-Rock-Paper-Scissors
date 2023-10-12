e7488c1aea52fb48c5a33e9b0b050293bdff8b47
John <78293728+Tostibrand@users.noreply.github.com>
2023-10-12 23:04:53 +0200

    Remove unnecessary pseudo code

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -20,44 +20,31 @@ function getComputerChoice(){
     }
 }
 
-// function playRound, added 2 parameters. defined getComputerChoice as the default option for computerSelection
+// function playRound, added 2 parameters. I set the output from the getComputerChoice function as the default value for computerSelection.
+// no need to pass computerSelection manually anymore now! just leave second parameter blank.
 function playRound(playerSelection, computerSelection = getComputerChoice()){
 const rock = `rock`
 const paper = `paper`
 const scissors = `scissors`
     
-// if playerSelection == computerSelection
-// return `tie`
 if (playerSelection == computerSelection){
     return `tie`
 }
-// if playerSelection is `rock` and computerSelection is `paper`
-// return `you lost`
 else if(playerSelection == rock && computerSelection == paper){
     return `paper beats rock, you lost!`
 }
-// if playerSelection is `paper` and computerSelection is `scissors`
-// return `you lost`
 else if(playerSelection == paper && computerSelection == scissors){
     return `scissors beats paper, you lose!`
 }
-// if playerSelection is `scissors` and computerSelection is `rock`
-// return `you lost`
 else if(playerSelection == scissors && computerSelection == rock){
     return `rock beats scissors, you lost!`
 }
-// if playerSelection is `rock` and computerSelection is `scissors`
-// return `you won`
 else if(playerSelection == rock && computerSelection == scissors){
     return `rock beats scissors, you won`
 }
-// if playerSelection is `scissors` and computerSelection is `paper`
-// return `you won`
 else if(playerSelection == scissors && computerSelection == paper){
     return `scissors beats paper, you won`
 }
-// if playerSelection is `paper` and computerSelection is `rock`
-// return `you won`
 else if(playerSelection == paper && computerSelection == rock){
     return `paper beats rock, you won`
 }
