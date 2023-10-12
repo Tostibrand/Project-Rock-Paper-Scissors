acf5449a2c2db10bcc482abe50c0736409dee9a9
John <78293728+Tostibrand@users.noreply.github.com>
2023-10-12 22:46:19 +0200

    Add psuedocode and convert to code 2 functions

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,1 +1,67 @@
-console.log(`hello world`)
+// console.log(`hello world`)
+
+// A function that will randomly get the computers choice
+
+// let getComputerChoice = Math.floor(Math.random()*3) + 1;
+
+function getComputerChoice(){
+    
+    const rock = `rock`
+    const paper = `paper`
+    const scissors = `scissors`
+    const number = Math.floor(Math.random()*3) + 1;
+    
+    if (number == 1){
+        return rock
+    } else if (number == 2) { 
+        return paper
+    } else {
+        return scissors
+    }
+}
+// const ai = getComputerChoice();
+
+function playRound(playerSelection, computerSelection = getComputerChoice()){
+let rock = `rock`
+let paper = `paper`
+let scissors = `scissors`
+    
+// if playerSelection == computerSelection
+// return `tie`
+if (playerSelection == computerSelection){
+    return `tie`
+}
+// if playerSelection is `rock` and computerSelection is `paper`
+// return `you lost`
+else if(playerSelection == rock && computerSelection == paper){
+    return `paper beats rock, you lost!`
+}
+// if playerSelection is `paper` and computerSelection is `scissors`
+// return `you lost`
+else if(playerSelection == paper && computerSelection == scissors){
+    return `scissors beats paper, you lose!`
+}
+// if playerSelection is `scissors` and computerSelection is `rock`
+// return `you lost`
+else if(playerSelection == scissors && computerSelection == rock){
+    return `rock beats scissors, you lost!`
+}
+// if playerSelection is `rock` and computerSelection is `scissors`
+// return `you won`
+else if(playerSelection == rock && computerSelection == scissors){
+    return `rock beats scissors, you won`
+}
+// if playerSelection is `scissors` and computerSelection is `paper`
+// return `you won`
+else if(playerSelection == scissors && computerSelection == paper){
+    return `scissors beats paper, you won`
+}
+// if playerSelection is `paper` and computerSelection is `rock`
+// return `you won`
+else if(playerSelection == paper && computerSelection == rock){
+    return `paper beats rock, you won`
+}
+}
+
+
+
