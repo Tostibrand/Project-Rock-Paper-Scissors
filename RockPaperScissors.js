264cfaad78fcbeaa6c81cc8fafe765c50ed417ab
John <78293728+Tostibrand@users.noreply.github.com>
2023-10-13 16:24:26 +0200

    Add status tracking and reset of game

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,9 +1,11 @@
-// console.log(`hello world`)
+// global variables that are set when the game (re)starts.
+let lost = 0
+let won = 0
+let tie = 0
+let round = 0
 
+// FUNCTION GETCOMPUTERCHOICE
 // A function that will randomly get the computers choice
-
-// let getComputerChoice = Math.floor(Math.random()*3) + 1;
-
 function getComputerChoice(){
     
     const rock = `rock`
@@ -20,36 +22,109 @@ function getComputerChoice(){
     }
 }
 
-// function playRound, added 2 parameters. I set the output from the getComputerChoice function as the default value for computerSelection.
-// no need to pass computerSelection manually anymore now! just leave second parameter blank.
-function playRound(playerSelection, computerSelection = getComputerChoice()){
+
+
+// FUNCTION PLAYROUND
+// all different outcomes to the game. Each out come updates the game status in between rounds
+function playRound(playerSelection = prompt(`Rock, paper or scissors?`), computerSelection = getComputerChoice()){
 const rock = `rock`
 const paper = `paper`
 const scissors = `scissors`
-
     
 if (playerSelection.toLowerCase() == computerSelection){
-    return `tie`
+        tie++;
+        round++;
+        console.log(`It's a tie!`);
+        console.log(`wins:`+ won);
+        console.log(`losses:`+ lost);
+        console.log(`ties:`+ tie);
+        console.log(`round:`+ round)
+        return;
 }
 else if(playerSelection.toLowerCase() == rock && computerSelection == paper){
-    return `paper beats rock, you lost!`
+        lost++;
+        round++;
+        console.log(`paper beats rock, you lost:`);
+        console.log(`wins:`+ won);
+        console.log(`losses:`+ lost);
+        console.log(`ties:`+ tie);
+        console.log(`round:`+ round)
+        return;
 }
 else if(playerSelection.toLowerCase() == paper && computerSelection == scissors){
-    return `scissors beats paper, you lose!`
+      lost++;
+      round++;
+      console.log(`paper beats rock, you lost:`);
+      console.log(`wins:`+ won);
+      console.log(`losses:`+ lost);
+      console.log(`ties:`+ tie);
+      console.log(`round:`+ round)
+      return;
 }
 else if(playerSelection.toLowerCase() == scissors && computerSelection == rock){
-    return `rock beats scissors, you lost!`
+      lost++;
+      round++;
+      console.log(`rock beats scissors, you lost:`);
+      console.log(`wins:`+ won);
+      console.log(`losses:`+ lost);
+      console.log(`ties:`+ tie);
+      console.log(`round:`+ round)
+      return;
 }
 else if(playerSelection.toLowerCase() == rock && computerSelection == scissors){
-    return `rock beats scissors, you won`
+      won++;
+      round++;
+      console.log(`rock beats scissors, you won:`);
+      console.log(`wins:`+ won);
+      console.log(`losses:`+ lost);
+      console.log(`ties:`+ tie);
+      console.log(`round:`+ round)
+      return;
+   
 }
 else if(playerSelection.toLowerCase() == scissors && computerSelection == paper){
-    return `scissors beats paper, you won`
+      won++;
+      round++;
+      console.log(`You won! scissors beats paper:`);
+      console.log(`wins:`+ won);
+      console.log(`losses:`+ lost);
+      console.log(`ties:`+ tie);
+      console.log(`round:`+ round)
+      return;
 }
 else if(playerSelection.toLowerCase() == paper && computerSelection == rock){
-    return `paper beats rock, you won`
+        won++;
+        round++;
+        console.log(`paper beats rock, you won`);
+        console.log(`wins:`+ won);
+        console.log(`losses:`+ lost);
+        console.log(`ties:`+ tie);
+        console.log(`round:`+ round)
+        return;
 }
 }
 
+// FUNCTION GAME
+function game(){
+// defines how many round there will be
+for(i = 0; i < 5; i++){
+    playRound()
+    }
+// evaluates the state of the game after 5 rounds and determines a winner
+if (won > lost){
+    console.log(`Game over, you won!`)
+} else if (won < lost){
+    console.log(`Game over, you lost!`)
+} else {
+    console.log(`Game over, it's a tie!`)
+}
+// resets the game after the loop is finished
+{
+    won = 0;
+    lost = 0;
+    ties = 0;
+    round = 0;
+}
+}
 
 
