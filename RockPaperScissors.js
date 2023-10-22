0dd4cea7ba7eb3f4aa5790ed9039487333ecd9c1
John <78293728+Tostibrand@users.noreply.github.com>
2023-10-22 23:02:50 +0200

    Add html, css and js code

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -4,6 +4,25 @@ let won = 0
 let tie = 0
 let round = 0
 
+
+// link elements to a const for JS
+const rock = document.querySelector(`#rockSelection`)
+const paper = document.querySelector(`#paperSelection`)
+const scissors = document.querySelector(`#scissorsSelection`)
+const computerScore = document.querySelector(`#computerScore`)
+const yourScore = document.querySelector(`#yourScore`)
+const roundScore = document.querySelector(`#roundScore`)
+const tieScore = document.querySelector(`#tieScore`)
+const text = document.querySelector(`#text`)
+const resetButton = document.querySelector(`#resetButton`)
+
+
+// EVENT LISTENERS WHICH CALL PLAYROUND FUNCTION
+rock.addEventListener(`click`, () => {playRound(`rock`)})
+paper.addEventListener(`click`, () => {playRound(`paper`)})
+scissors.addEventListener(`click`, () => {playRound(`scissors`)})
+resetButton.addEventListener(`click`, () => {resetGame()})
+
 // FUNCTION GETCOMPUTERCHOICE
 // A function that will randomly get the computers choice
 function getComputerChoice(){
@@ -22,122 +41,89 @@ function getComputerChoice(){
     }
 }
 
+// FUNCTION RESET GAME
+function resetGame(){
+    lost = 0
+    won = 0
+    tie = 0
+    round = 0
+    text.textContent = `Let's Play!`
+    roundScore.textContent = 0;
+    yourScore.textContent = 0;
+    computerScore.textContent = 0;
+    tieScore.textContent = 0;
 
-
+}
 // FUNCTION PLAYROUND
 // all different outcomes to the game. Each out come updates the game status in between rounds
-function playRound(playerSelection = prompt(`Welcome, lets play a game of Rock, paper or scissors! put you answer down below and good luck!`), computerSelection = getComputerChoice()){
-const rock = `rock`
-const paper = `paper`
-const scissors = `scissors`
-    
-if (playerSelection.toLowerCase() == computerSelection){
-        tie++;
-        round++;
-        console.log(`It's a tie!`);
-        console.log(`wins:`+ won);
-        console.log(`losses:`+ lost);
-        console.log(`ties:`+ tie);
-        console.log(`round:`+ round)
-        return;
-}
-else if(playerSelection.toLowerCase() == rock && computerSelection == paper){
-        lost++;
-        round++;
-        console.log(`paper beats rock, you lost:`);
-        console.log(`wins:`+ won);
-        console.log(`losses:`+ lost);
-        console.log(`ties:`+ tie);
-        console.log(`round:`+ round)
-        return;
-}
-else if(playerSelection.toLowerCase() == paper && computerSelection == scissors){
-      lost++;
-      round++;
-      console.log(`paper beats rock, you lost:`);
-      console.log(`wins:`+ won);
-      console.log(`losses:`+ lost);
-      console.log(`ties:`+ tie);
-      console.log(`round:`+ round)
-      return;
-}
-else if(playerSelection.toLowerCase() == scissors && computerSelection == rock){
-      lost++;
-      round++;
-      console.log(`rock beats scissors, you lost:`);
-      console.log(`wins:`+ won);
-      console.log(`losses:`+ lost);
-      console.log(`ties:`+ tie);
-      console.log(`round:`+ round)
-      return;
-}
-else if(playerSelection.toLowerCase() == rock && computerSelection == scissors){
-      won++;
-      round++;
-      console.log(`rock beats scissors, you won:`);
-      console.log(`wins:`+ won);
-      console.log(`losses:`+ lost);
-      console.log(`ties:`+ tie);
-      console.log(`round:`+ round)
-      return;
-   
-}
-else if(playerSelection.toLowerCase() == scissors && computerSelection == paper){
-      won++;
-      round++;
-      console.log(`You won! scissors beats paper:`);
-      console.log(`wins:`+ won);
-      console.log(`losses:`+ lost);
-      console.log(`ties:`+ tie);
-      console.log(`round:`+ round)
-      return;
-}
-else if(playerSelection.toLowerCase() == paper && computerSelection == rock){
-        won++;
-        round++;
-        console.log(`paper beats rock, you won`);
-        console.log(`wins:`+ won);
-        console.log(`losses:`+ lost);
-        console.log(`ties:`+ tie);
-        console.log(`round:`+ round)
-        return;
-}
-}
+function playRound(playerSelection, computerSelection = getComputerChoice()){
+    const rock = `rock`
+    const paper = `paper`
+    const scissors = `scissors`
 
-
-
-// FUNCTION GAME
-function game(){
-// defines how many round there will be
-for(i = 0; i < 5; i++){
-    playRound()
+    if (won > lost && round == 5){
+        text.textContent = `Game over, you won!`
+    } else if (won < lost && round == 5){
+        text.textContent = `Game over, you lost!`
+    } else if (won == lost && round >= 5) {
+        text.textContent =`Game over, it's a tie!`
+    }else {
+        
+    if (playerSelection.toLowerCase() == computerSelection){
+            tie++;
+            round++;
+            tieScore.textContent = tie;   
+            roundScore.textContent = round;
+            text.textContent = `It's a tie!`
+            return;
     }
-
-
-
-// evaluates the state of the game after 5 rounds and determines a winner
-if (won > lost){
-    console.log(`Game over, you won!`)
-} else if (won < lost){
-    console.log(`Game over, you lost!`)
-} else {
-    console.log(`Game over, it's a tie!`)
-}
-
-
-
-// resets the game after the loop is finished
-{
-    won = 0;
-    lost = 0;
-    tie = 0;
-    round = 0;
-    console.log(`Game reset`)
-    console.log(`Thank you for playing!`)
-}
-
-}
-
-
-
-
+    else if(playerSelection.toLowerCase() == rock && computerSelection == paper){
+            lost++;
+            round++;            
+            computerScore.textContent = lost;
+            roundScore.textContent = round;
+            text.textContent = `You lost!`                    
+            return;
+    }
+    else if(playerSelection.toLowerCase() == paper && computerSelection == scissors){
+          lost++;
+          round++;          
+          computerScore.textContent = lost;
+          roundScore.textContent = round;
+          text.textContent = `You lost!`
+          return;
+    }
+    else if(playerSelection.toLowerCase() == scissors && computerSelection == rock){
+          lost++;
+          round++;         
+          computerScore.textContent = lost; 
+          roundScore.textContent = round;
+          text.textContent = `You lost!`
+          return;
+    }
+    else if(playerSelection.toLowerCase() == rock && computerSelection == scissors){
+          won++;
+          round++;         
+          yourScore.textContent = won;
+          roundScore.textContent = round;
+          text.textContent = `You won!`
+          return;
+       
+    }
+    else if(playerSelection.toLowerCase() == scissors && computerSelection == paper){
+          won++;
+          round++;         
+          yourScore.textContent = won;
+          roundScore.textContent = round;
+          text.textContent = `You won!`
+          return;
+    }
+    else if(playerSelection.toLowerCase() == paper && computerSelection == rock){
+            won++;
+            round++;           
+            yourScore.textContent = won;
+            roundScore.textContent = round;
+            text.textContent = `You won!`
+            return;
+    }
+    }}
